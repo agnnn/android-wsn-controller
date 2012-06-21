0947e466913984d39d26ef0ea5980564e8adc15e
stephanherold87 <stephanherold87@googlemail.com>
2012-06-21 17:43:23 +0000

    added control transfer...

--- src/de/rwth/comsys/AndroidWSNControllerActivity.java
+++ src/de/rwth/comsys/AndroidWSNControllerActivity.java
@@ -2,6 +2,7 @@ package de.rwth.comsys;
 
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteBuffer;
@@ -14,6 +15,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.usb.UsbAccessory;
 import android.hardware.usb.UsbConstants;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
@@ -27,6 +29,9 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import java.io.*;
+
+import de.rwth.comsys.Enums.FTDI_Constants;
 
 public class AndroidWSNControllerActivity extends Activity {
 
@@ -37,6 +42,7 @@ public class AndroidWSNControllerActivity extends Activity {
 	private UsbEndpoint receivingEndpointMSP430 = null;	//view of HOST
 	private TextView textView = null;
 	private PendingIntent mPermissionIntent = null;
+	UsbInterface mUSBInterface = null;
 	
 
 	/** Called when the activity is first created. */
@@ -44,17 +50,16 @@ public class AndroidWSNControllerActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
-		// get button and register listener
+				// get button and register listener
 		Button myButtonConnect = (Button) findViewById(R.id.button);
 		myButtonConnect.setOnClickListener(buttonConnectListener);
 		Button myButtonSend = (Button) findViewById(R.id.button1);
 		myButtonSend.setOnClickListener(buttonSendListener);
 		textView =   (TextView) findViewById(R.id.textView);
 		
-		
 		// retrieve USB Service
 		mManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+		
 		
 		// listen for new devices
 		mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
@@ -66,9 +71,10 @@ public class AndroidWSNControllerActivity extends Activity {
 	// OnClickListener iterates over connected devices and requests permission
 	private OnClickListener buttonConnectListener = new OnClickListener() {
 		public void onClick(View v) {
-
+			
 			HashMap<String, UsbDevice> deviceList = mManager.getDeviceList();
 
+			
 			if (deviceList.isEmpty())
 				textView.append("Nothing found! \n");
 
@@ -77,6 +83,7 @@ public class AndroidWSNControllerActivity extends Activity {
 			{
 				mDevice = deviceIterator.next();
 				mManager.requestPermission(mDevice, mPermissionIntent);
+				textView.append("device: "+mDevice.getDeviceName()+" found\n");
 			}
 		}
 	};
@@ -87,8 +94,17 @@ public class AndroidWSNControllerActivity extends Activity {
 			
 			if(mDeviceConnection != null){
 				
-				byte[] startSeq = new byte[2];
-				startSeq[0] = (byte) 128;
+				resetTelosb(true);
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				byte[] startSeq = new byte[1];
+				startSeq[0] = (byte) 0x80;
+				//startSeq[1] = (byte) 0x01;
+				//startSeq[2] = (byte) 0x80;
 				
 				ByteBuffer startSeqRespBuf = ByteBuffer.allocate(2);
 				UsbRequest startSeqResponse = new UsbRequest();
@@ -96,18 +112,24 @@ public class AndroidWSNControllerActivity extends Activity {
 				startSeqResponse.queue(startSeqRespBuf, 2);
 				
 				mDeviceConnection.bulkTransfer(sendingEndpointMSP430, startSeq, 1, 200);
-				textView.append("sent: 0x"+Integer.toHexString((byte)startSeq[0])+"\n");
+				textView.append("sent1: 0x"+Integer.toHexString((byte)startSeq[0])+"\n");
+				//textView.append("sent2: 0x"+Integer.toHexString((byte)startSeq[1])+"\n");
+				//textView.append("sent3: 0x"+Integer.toHexString((byte)startSeq[2])+"\n");
 				ByteConverter.bla(textView);
 				if (mDeviceConnection.requestWait() == startSeqResponse) {
+					textView.append("orig result: \n");
+					textView.append("1: 0x"+Integer.toHexString(startSeqRespBuf.get(0))+"\n");
+					textView.append("2: 0x"+Integer.toHexString(((short)startSeqRespBuf.get(1)) & 0x00FF)+"\n");
+					//textView.append("3: 0x"+Integer.toHexString(((short)startSeqRespBuf.get(2)) & 0x00FF)+"\n");
 					short[] res = ByteConverter.getByteStreamFromSigned(startSeqRespBuf);
 					textView.append("result init: 0x"+Integer.toHexString(res[0])+"\n");
-					//textView.append("res: "+String.valueOf(res[0]));
+					textView.append("res: "+String.valueOf(res[0]));
 					/*for(int j = 0; j< startSeqRespBuf.capacity(); j++){
 						textView.append(startSeqRespBuf.get()+"\n");
 					}*/
 				}
 
-				ByteBuffer buffer = ByteBuffer.allocate(2);
+			/*	ByteBuffer buffer = ByteBuffer.allocate(2);
 		        UsbRequest response = new UsbRequest();
 		        response.initialize(mDeviceConnection, receivingEndpointMSP430);
 		        response.queue(buffer, 2);
@@ -129,6 +151,7 @@ public class AndroidWSNControllerActivity extends Activity {
 				{
 					textView.append("0x"+Integer.toHexString(res[i])+",");
 				}
+				*/
 				/*
 				 *  set password cmd to unlock pw protected commands
 				 */
@@ -159,7 +182,7 @@ public class AndroidWSNControllerActivity extends Activity {
 				/*
 				 *  read bsl version
 				 */				
-				
+				/*
 				ByteBuffer buffer3 = ByteBuffer.allocate(32);
 		        UsbRequest response3 = new UsbRequest();
 		        response3.initialize(mDeviceConnection, receivingEndpointMSP430);
@@ -167,7 +190,6 @@ public class AndroidWSNControllerActivity extends Activity {
 		        // Request to mass erase
 		        ByteBuffer readVersion = getTransmitBslVersionCmd();
 				mDeviceConnection.bulkTransfer(sendingEndpointMSP430, readVersion.array(), readVersion.capacity(), 200); //do in another thread
-				
 			
 				
 				if (mDeviceConnection.requestWait() == response3) {
@@ -184,7 +206,7 @@ public class AndroidWSNControllerActivity extends Activity {
 				for(int i=0;i<res3.length;i++)
 				{
 					textView.append("0x"+Integer.toHexString(res3[i])+",");
-				}
+				}*/
 			}
 		}
 	};
@@ -336,8 +358,6 @@ public class AndroidWSNControllerActivity extends Activity {
 			return;
 		}
 
-		UsbInterface mUSBInterface = null;
-		
 		mUSBInterface = device.getInterface(0);
 
 		// MSP430 has 2 endpoints (SLAU319B.pdf, paragraph 1.5)
@@ -345,11 +365,13 @@ public class AndroidWSNControllerActivity extends Activity {
 			textView.append("Can't find all endpoints!\n");
 			return;
 		}
+		textView.append("interfaceId: "+mUSBInterface.getId()+"\n");
 
 		
 		// endpoint should be of type HID
 		UsbEndpoint endpoint0 = mUSBInterface.getEndpoint(0);
 		UsbEndpoint endpoint1 = mUSBInterface.getEndpoint(1);
+		
 		
 //		textView.append("\n Endpoint 0: "+"\n" +endpoint0+"\n Type: "+endpoint0.getType() +"\n");
 //		textView.append("\n Endpoint 1: "+"\n" +endpoint1+"\n Type: "+endpoint1.getType() +"\n");
@@ -438,5 +460,160 @@ public class AndroidWSNControllerActivity extends Activity {
 	public void onDestroy() {
 		super.onDestroy();
 		// TODO release device
+	}
+	
+	private void resetTelosb(boolean invokeBsl) {
+		if(invokeBsl)
+		{
+			telosWriteCmd((byte)0,(byte)1);
+			telosWriteCmd((byte)0,(byte)3);
+			telosWriteCmd((byte)0,(byte)1);
+			telosWriteCmd((byte)0,(byte)3);
+			telosWriteCmd((byte)0,(byte)2);
+			telosWriteCmd((byte)0,(byte)0);
+		}
+		else{
+			telosWriteCmd((byte)0,(byte)3);
+			telosWriteCmd((byte)0,(byte)2);
+			telosWriteCmd((byte)0,(byte)0);
+	        try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	private void telosStop() {
+		setDTR(true);
+		setRTS(false);
+		setDTR(false);
+	}
+
+	private void telosWriteByte(byte dataByte) {
+		telosWriteBit( ( dataByte & 0x80) > 0 );
+        telosWriteBit( ( dataByte & 0x40) > 0 );
+        telosWriteBit( ( dataByte & 0x20) > 0);
+        telosWriteBit( ( dataByte & 0x10) > 0);
+        telosWriteBit( ( dataByte & 0x08) > 0);
+        telosWriteBit( ( dataByte & 0x04) > 0);
+        telosWriteBit( ( dataByte & 0x02) > 0);
+        telosWriteBit( ( dataByte & 0x01) > 0);
+        telosWriteBit( false );  // "acknowledge"
+	}
+
+	private void telosStart() {
+		setDTR(false);
+		setRTS(false);
+		setDTR(true);
+	}
+
+	/**
+	 * Sets the dtr.
+	 *
+	 * @param dtr: DTR setting. Can only be SIO_SET_DTR_HIGH or SIO_SET_DTR_LOW.
+	 * @return 0: Everything is OK.
+	 *  -1: USB controlTransfer method failed.
+	 *  -2: input value cannot be recognized.
+	 */
+	private int setDTR(boolean dtr)
+	{
+		int iDtr = FTDI_Constants.SIO_SET_DTR_LOW;
+		if(dtr == true)
+			iDtr = FTDI_Constants.SIO_SET_DTR_HIGH;
+		switch(iDtr)
+		{
+		case FTDI_Constants.SIO_SET_DTR_HIGH:
+		case FTDI_Constants.SIO_SET_DTR_LOW:
+			break;
+		default:
+			Log.e("ftdi_control","The DTR value can only be SIO_SET_DTR_HIGH or SIO_SET_DTR_LOW: "+ Integer.toString(iDtr));
+			return -2;
+		}
+		int r;
+		if((r = mDeviceConnection.controlTransfer(FTDI_Constants.FTDI_DEVICE_OUT_REQTYPE, 
+													 FTDI_Constants.SIO_SET_MODEM_CTRL_REQUEST,
+													 iDtr,
+													 mUSBInterface.getId(),
+													 null,
+													 0, 
+													 2000)) != 0)
+		{
+			Log.e("ftdi_control","USB controlTransfer operation failed. controlTransfer return value is:"+Integer.toString(r));
+			//textView.append(Integer.toString(-1)+",");
+			return -1;
+		}
+		else
+		{
+			//textView.append(Integer.toString(0)+",");
+			return 0;
+		}
+	}
+	
+	/**
+	 * Sets the rts.
+	 *
+	 * @param rts: the RTS setting. Can only be SIO_SET_RTS_HIGH or SIO_SET_RTS_LOW.
+	 * @return 0: Everything is OK.
+	 *  -1: USB controlTransfer method failed.
+	 *  -2: input value cannot be recognized.
+	 */
+	private int setRTS(boolean rts)
+	{
+		int iRts = FTDI_Constants.SIO_SET_RTS_LOW;
+		if(rts == true)
+			iRts = FTDI_Constants.SIO_SET_RTS_HIGH;
+		switch(iRts)
+		{
+		case FTDI_Constants.SIO_SET_RTS_HIGH:
+		case FTDI_Constants.SIO_SET_RTS_LOW:
+			break;
+		default:
+			Log.e("ftdi_control","The RTS value can only be SIO_SET_RTS_HIGH or SIO_SET_RTS_LOW: "+ Integer.toString(iRts));
+			return -2;
+		}
+		int r;
+		if((r = mDeviceConnection.controlTransfer(FTDI_Constants.FTDI_DEVICE_OUT_REQTYPE, 
+												  FTDI_Constants.SIO_SET_MODEM_CTRL_REQUEST,
+												  iRts, mUSBInterface.getId(), null, 0, 2000)) != 0)
+		{
+			//textView.append(Integer.toString(-1)+",");
+			Log.e("ftdi_control","USB controlTransfer operation failed. controlTransfer return value is:"+Integer.toString(r));
+			return -1;
+		}
+		else
+		{
+			//textView.append(Integer.toString(0)+",");
+			return 0;
+		}
+		
+	}
+
+	private void telosWriteBit(boolean bit)
+	{
+		try
+		{
+			setRTS(true);
+			setDTR(!bit);
+			Thread.sleep(0,002);
+			setRTS(false);
+			Thread.sleep(0,001);
+			setRTS(true);
+		} 
+		catch (InterruptedException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void telosWriteCmd(byte addr, byte cmd)
+	{
+		telosStart();
+		telosWriteByte((byte)(0x90 | (addr << 1)));
+		telosWriteByte(cmd);
+		telosStop();
 	}
 }
