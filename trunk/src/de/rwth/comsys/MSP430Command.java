e662b3d2fe51f5a80d17f73ba89a36412fc2df81
mailtotheis@gmail.com <mailtotheis@gmail.com@842361ae-13d9-ffc3-ab2c-1d8eb7f94f8e>
2012-06-30 14:37:45 +0000

    Load PC command...

--- trunk/src/de/rwth/comsys/MSP430Command.java
+++ trunk/src/de/rwth/comsys/MSP430Command.java
@@ -7,30 +7,35 @@ import de.rwth.comsys.Enums.MSP430_Command;
 public class MSP430Command {
 	private MSP430_Command command;
 	private byte[] data;
-	private short startAddress;
-	private short endAddress;
+	private int startAddress;
+	
 	private ArrayList<Record> records;
 	
 	
-	public MSP430Command(MSP430_Command command, byte[] data, short startAddress, short endAddress, ArrayList<Record> records ) {
+	public MSP430Command(MSP430_Command command, byte[] data, int startAddress, ArrayList<Record> records ) {
 		this.command = command;
 		this.data = data;
-		this.startAddress = startAddress;
-		this.endAddress = endAddress;
+		this.setStartAddress(startAddress);
 		this.records = (records);
 	}
 	
 	public MSP430Command(MSP430_Command command) {
-		this(command,null,(short)0,(short)0, null);
+		this(command, null, 0, null);
 	}
 	
 	public MSP430Command(MSP430_Command command, byte[] data) {
-		this(command,data,(short)0,(short)0, null);
+		this(command, data, 0, null);
+	}
+	
+	public MSP430Command(MSP430_Command command, int startAddress) {
+		this(command, null, startAddress, null);
 	}
 	
 	public MSP430Command(MSP430_Command command, ArrayList<Record> records) {
-		this(command, null, (short)0, (short)0, records);
+		this(command, null, 0, records);
 	}
+	
+	
 	
 	/**
 	 * @return the command
@@ -38,18 +43,21 @@ public class MSP430Command {
 	public MSP430_Command getCommand() {
 		return command;
 	}
+	
 	/**
 	 * @param command the command to set
 	 */
 	public void setCommand(MSP430_Command command) {
 		this.command = command;
 	}
+	
 	/**
 	 * @return the data
 	 */
 	public byte[] getData() {
 		return data;
 	}
+	
 	/**
 	 * @param data the data to set
 	 */
@@ -59,27 +67,7 @@ public class MSP430Command {
 	/**
 	 * @return the startAddress
 	 */
-	public short getStartAddress() {
-		return startAddress;
-	}
-	/**
-	 * @param startAddress the startAddress to set
-	 */
-	public void setStartAddress(short startAddress) {
-		this.startAddress = startAddress;
-	}
-	/**
-	 * @return the endAddress
-	 */
-	public short getEndAddress() {
-		return endAddress;
-	}
-	/**
-	 * @param endAddress the endAddress to set
-	 */
-	public void setEndAddress(short endAddress) {
-		this.endAddress = endAddress;
-	}
+	
 
 	/**
 	 * @return the records
@@ -93,6 +81,20 @@ public class MSP430Command {
 	 */
 	public void setRecords(ArrayList<Record> records) {
 		this.records = records;
+	}
+
+	/**
+	 * @return the startAddress
+	 */
+	public int getStartAddress() {
+		return startAddress;
+	}
+
+	/**
+	 * @param startAddress the startAddress to set
+	 */
+	public void setStartAddress(int startAddress) {
+		this.startAddress = startAddress;
 	}
 	
 	
