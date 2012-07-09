6b98a274c50ee2cc33eb85152232ffe88694bf2c
mailtotheis <mailtotheis@gmail.com>
2012-07-09 07:38:27 +0000

    ELFLoader

---
+++ src/de/rwth/comsys/elf/ElfLoader.java
@@ -0,0 +1,637 @@
+package de.rwth.comsys.elf;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * Loads a ELF file and parses the file. Limited to files smaller than the max value of
+ * Integers in Bytes.
+ * 
+ * @author Christian & Stephan
+ * 
+ */
+public class ElfLoader
+{
+
+	private byte[] loadedFile = null;
+	private Header header = null;
+	private ArrayList<SectionHeader> sectionHeaders = null;
+	private ArrayList<byte[]> sectionDataArrays = null;
+	private HashMap<Integer, String> sectionHeaderNames = null;
+	private HashMap<Integer, String> symbolTableEntryNames = null;
+	private ArrayList<SymbolTableEntry> symbolTableEntries = null;
+
+
+
+
+	/**
+	 * Loads a ELF file and parses the file. Sets attributes if nothing goes wrong.
+	 * 
+	 * @param pathName
+	 * 
+	 */
+	public ElfLoader(String pathName)
+	{
+		int offset = 0;
+		int sectionLength = 0;
+		File file = new File(pathName);
+		FileInputStream input;
+
+		try
+		{
+			input = new FileInputStream(file);
+
+		} catch (Exception e)
+		{
+			return;
+		}
+
+		// should never happen, because we are dealing with MCUs
+		if (file.length() > Integer.MAX_VALUE)
+		{
+			errorHandler(ELFErrorCode.ELF_FILE_TOO_LARGE);
+			return;
+		}
+
+		byte[] buffer = new byte[(int) file.length()];
+
+		try
+		{
+			// fill buffer
+			if (input.read(buffer) == -1)
+				return;
+
+			input.close();
+
+		} catch (Exception e)
+		{
+			return;
+		}
+
+		loadedFile = buffer;
+
+		// parse header
+		header = parseElfHeader();
+		if (header == null || !header.hasNoNullAttribute())
+		{
+			errorHandler(ELFErrorCode.ELF_PARSE_HEADER_ERROR);
+			return;
+		}
+
+		// parse section headers
+		sectionHeaders = parseSectionHeader((int) header.getSectionHeaderTableFileOffset(),
+				header.getSectionHeaderTableEntrySize() * header.getSectionHeaderTableEntryCount(),
+				header.getSectionHeaderTableEntrySize());
+		if (header == null)
+		{
+			errorHandler(ELFErrorCode.ELF_PARSE_SECTION_HEADERS_ERROR);
+			return;
+		}
+
+		// parse section header names
+		SectionHeader stringTableSectionHeader = sectionHeaders.get(header.getSectionHeaderStringTableIndex());
+		offset = (int) stringTableSectionHeader.getSectionFileOffset();
+		sectionLength = (int) stringTableSectionHeader.getSectionSize();
+		sectionHeaderNames = parseStringTable(offset, sectionLength);
+		if (sectionHeaderNames == null)
+		{
+			errorHandler(ELFErrorCode.ELF_PARSE_SECTION_HEADERS_NAMES_ERROR);
+			return;
+		}
+
+		// solve name by offsetInStringTable and sectionHeaderNames
+		for (Iterator<SectionHeader> iterator = sectionHeaders.iterator(); iterator.hasNext();)
+		{
+			SectionHeader currentSectionHeader = iterator.next();
+			Integer key = (int) (stringTableSectionHeader.getSectionFileOffset() + currentSectionHeader
+					.getOffsetInStringTable());
+			currentSectionHeader.setName(sectionHeaderNames.get(key));
+		}
+
+		// search symbolTableSection by symbolTableHeader
+		SectionHeader symbolTableSectionHeader = null;
+		String symtab = ".symtab";
+		for (Iterator<SectionHeader> iterator = sectionHeaders.iterator(); iterator.hasNext();)
+		{
+			SectionHeader currentSectionHeader = iterator.next();
+
+			if (symtab.equals(currentSectionHeader.getName()))
+			{
+				symbolTableSectionHeader = currentSectionHeader;
+				break;
+			}
+		}
+		if (symbolTableSectionHeader == null)
+		{
+			errorHandler(ELFErrorCode.ELF_SYMTABLE_SECTION_HEADER_NOT_FOUND_ERROR);
+			return;
+		}
+
+		// parse symbolTable Section
+		offset = (int) symbolTableSectionHeader.getSectionFileOffset();
+		sectionLength = (int) symbolTableSectionHeader.getSectionSize();
+		int symbolTableEntrySize = (int) symbolTableSectionHeader.getFixedTableEntrySize();
+		symbolTableEntries = parseSymbolTable(offset, sectionLength, symbolTableEntrySize);
+		if (symbolTableEntries == null)
+		{
+			errorHandler(ELFErrorCode.ELF_PARSE_SYMTABLE_ERROR);
+			return;
+		}
+
+		// search symbolTableNameSection by symbolTableNameHeader
+		SectionHeader symbolTableNameHeader = null;
+		String symbolTableNameSectionName = ".strtab";
+		for (Iterator<SectionHeader> iterator = sectionHeaders.iterator(); iterator.hasNext();)
+		{
+			SectionHeader currentSectionHeader = iterator.next();
+
+			if (symbolTableNameSectionName.equals(currentSectionHeader.getName()))
+			{
+				symbolTableNameHeader = currentSectionHeader;
+				break;
+			}
+		}
+		if (symbolTableNameHeader == null)
+		{
+			errorHandler(ELFErrorCode.ELF_SYMTABLE_NAMES_SECTION_HEADER_NOT_FOUND_ERROR);
+			return;
+		}
+
+		// parse symbolTableNames
+		offset = (int) symbolTableNameHeader.getSectionFileOffset();
+		sectionLength = (int) symbolTableNameHeader.getSectionSize();
+		symbolTableEntryNames = parseStringTable(offset, sectionLength);
+		if (symbolTableEntryNames == null)
+		{
+			errorHandler(ELFErrorCode.ELF_PARSE_SYMBTABLE_ENTRY_NAMES_ERROR);
+			return;
+		}
+
+		// solve name by offsetInStringTable and sectionHeaderNames
+		for (Iterator<SymbolTableEntry> iterator = symbolTableEntries.iterator(); iterator.hasNext();)
+		{
+			SymbolTableEntry currentSymbolTableEntry = iterator.next();
+			Integer key = (int) (symbolTableNameHeader.getSectionFileOffset() + currentSymbolTableEntry
+					.getOffsetInStringTable());
+			String name = symbolTableEntryNames.get(key);
+			currentSymbolTableEntry.setName(name);
+		}
+		
+		// fill data arrays
+		
+		
+	}
+
+
+
+
+	/**
+	 * Calculates position in loadedFile of a variable by name.
+	 * 
+	 * @param name
+	 * @param sizeInBytes
+	 * @return null or Variable
+	 */
+	public Variable getVariableBySymbolTableName(String name)
+	{
+		if (symbolTableEntries == null || sectionHeaders == null || name == "")
+			return null;
+
+		Variable variable = null;
+
+		SymbolTableEntry searchedEntry = null;
+
+		for (Iterator<SymbolTableEntry> iterator = symbolTableEntries.iterator(); iterator.hasNext();)
+		{
+			SymbolTableEntry currentSymbolTableEntry = iterator.next();
+			String currentName = currentSymbolTableEntry.getName();
+			if (currentName != null && currentName.equals(name))
+			{
+				searchedEntry = currentSymbolTableEntry;
+				break;
+			}
+		}
+
+		// search file offset of name
+		if (searchedEntry != null)
+		{
+			// belongs to section ?
+			SectionHeader sectionHeader = sectionHeaders.get(searchedEntry.getSectionIndex());
+
+			// calculate offset in file
+			int offset = (int) (sectionHeader.getSectionFileOffset() + (searchedEntry.getValue() - sectionHeader
+					.getAddress()));
+			int size = (int) searchedEntry.getSize();
+			variable = new Variable(name, offset, size, (int) getValue(offset, size));
+
+		}
+
+		return variable;
+	}
+
+
+
+
+	/**
+	 * Manipulates loadedFile by name and value. Stores the loadedFile in a new file with
+	 * the given filename at the given path.
+	 * 
+	 * @param path
+	 * @param filename
+	 * @param name of entry in symbolTable
+	 * @param value to write
+	 * @return true if writing was successful
+	 */
+	public boolean storeValueInFileByName(String path, String filename, String name, int value)
+	{
+		// get offset and size of entry
+		Variable var = getVariableBySymbolTableName(name);
+		if (var == null || var.hasNoNullAttribute() == false
+				|| (loadedFile.length < var.getSizeInBytes() + var.getFileOffset()))
+			return false;
+
+		// transform value to little endian format
+		byte[] valuesToWrite = createLittleEndianIntArrayByValue(value);
+
+		// compare size of old value with new value
+		int size = 0;
+		for (int i = 0; i < valuesToWrite.length; i++)
+		{
+			if (valuesToWrite[i] != 0)
+			{
+				size++;
+			}
+
+		}
+		// ensure to not run out of range and to overwrite all bytes
+		if (size > var.getSizeInBytes() || var.getSizeInBytes() > 4)
+			return false;
+
+		File file = new File(path + filename);
+
+		// overwrite up to 4 bytes
+		for (int i = 0; i < valuesToWrite.length && i < var.getSizeInBytes(); i++)
+		{
+			byte currentByte = valuesToWrite[i];
+			loadedFile[var.getFileOffset() + i] = currentByte;
+		}
+
+		try
+		{
+
+			boolean ok = file.createNewFile();
+			if (ok == false)
+			{
+				return false;
+			}
+
+			FileOutputStream output = new FileOutputStream(file);
+			output.write(loadedFile, 0, loadedFile.length);
+			output.close();
+
+		} catch (Exception e)
+		{
+			return false;
+		}
+		return true;
+	}
+
+
+
+
+	/**
+	 * Parses section headers of ELF. Parse ELF header at first to get sectionHeaderSize
+	 * etc..
+	 * 
+	 * @param offset
+	 * @param sectionLength
+	 * @param sectionHeaderSize
+	 * @return
+	 */
+	private ArrayList<SectionHeader> parseSectionHeader(int offset, int sectionLength, int sectionHeaderSize)
+	{
+		ArrayList<SectionHeader> sectionHeaders = new ArrayList<SectionHeader>();
+
+		for (int i = offset; (i < this.loadedFile.length) && (i < sectionLength + offset); i = i + sectionHeaderSize)
+		{
+			SectionHeader currentHeader = new SectionHeader();
+
+			currentHeader.setOffsetInStringTable(getValue(i, 4));
+			currentHeader.setType(getValue(i + 4, 4));
+			currentHeader.setFlags(getValue(i + 8, 4));
+			currentHeader.setAddress(getValue(i + 12, 4));
+			currentHeader.setSectionFileOffset(getValue(i + 16, 4));
+			currentHeader.setSectionSize(getValue(i + 20, 4));
+			currentHeader.setLinkToAnotherSection(getValue(i + 24, 4));
+			currentHeader.setInfo(getValue(i + 28, 4));
+			currentHeader.setAlignment(getValue(i + 32, 4));
+			currentHeader.setFixedTableEntrySize(getValue(i + 36, 4));
+
+			// parsed successful?
+			if (currentHeader.hasNoNullAttribute())
+			{
+				sectionHeaders.add(currentHeader);
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		return sectionHeaders;
+	}
+
+
+
+
+	/**
+	 * Parses header of ELF.
+	 * 
+	 * @param sectionHeaderByteArray
+	 * @param numberOfSections
+	 * @return header or null if error occurs
+	 */
+	private Header parseElfHeader()
+	{
+		if (loadedFile == null || loadedFile.length < 52)
+			return null;
+
+		Header newHeader = new Header();
+		short[] ident = newHeader.getIdent();
+
+		// fill ident array
+		for (int j = 0; j < (loadedFile.length) && (j < ident.length); j++)
+		{
+			ident[j] = (short) (loadedFile[j] & 0xFF);
+			// TODO check values ... correct elf file?
+		}
+
+		newHeader.setType((int) getValue(16, 2));
+		newHeader.setArchitecture((int) getValue(18, 2));
+		newHeader.setVersion(getValue(20, 4));
+		newHeader.setEntryPointAddress(getValue(24, 4));
+		newHeader.setProgramHeaderTableFileOffset(getValue(28, 4));
+		newHeader.setSectionHeaderTableFileOffset(getValue(32, 4));
+		newHeader.setFlags(getValue(36, 4));
+		newHeader.setHeaderSizeInBytes((int) getValue(40, 2));
+		newHeader.setProgramHeaderTableEntrySize((int) getValue(42, 2));
+		newHeader.setProgramHeaderTableEntryCount((int) getValue(44, 2));
+		newHeader.setSectionHeaderTableEntrySize((int) getValue(46, 2));
+		newHeader.setSectionHeaderTableEntryCount((int) getValue(48, 2));
+		newHeader.setSectionHeaderStringTableIndex((int) getValue(50, 2));
+
+		// parsed successful?
+		if (!newHeader.hasNoNullAttribute())
+		{
+			return null;
+		}
+
+		return newHeader;
+	}
+
+
+
+
+	/**
+	 * Parses a symbol table of an ELF file.
+	 * 
+	 * @param offset
+	 * @param symbolTableLength
+	 * @param symbolTableEntrySize
+	 * @return null or symbolTableEntries
+	 */
+	private ArrayList<SymbolTableEntry> parseSymbolTable(int offset, int symbolTableLength, int symbolTableEntrySize)
+	{
+		if (this.loadedFile == null)
+			return null;
+
+		ArrayList<SymbolTableEntry> symbolTableEntries = new ArrayList<SymbolTableEntry>();
+
+		for (int i = offset; i < this.loadedFile.length && (i < symbolTableLength + offset); i = i
+				+ symbolTableEntrySize)
+		{
+			SymbolTableEntry currentSymbolTableEntry = new SymbolTableEntry();
+
+			currentSymbolTableEntry.setOffsetInStringTable(getValue(i, 4));
+			currentSymbolTableEntry.setValue(getValue(i + 4, 4));
+			currentSymbolTableEntry.setSize(getValue(i + 8, 4));
+			currentSymbolTableEntry.setInfo((short) getValue(i + 12, 1));
+			currentSymbolTableEntry.setOther((short) getValue(i + 13, 1));
+			currentSymbolTableEntry.setSectionIndex((int) getValue(i + 14, 2));
+
+			// parsed successful?
+			if (currentSymbolTableEntry.hasNoNullAttribute())
+			{
+				symbolTableEntries.add(currentSymbolTableEntry);
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		return symbolTableEntries;
+
+	}
+
+
+
+
+	/**
+	 * Parses a String table section of ELF.
+	 * 
+	 * @return hashmap<offset in section, name>
+	 */
+	private HashMap<Integer, String> parseStringTable(int offset, int tableLength)
+	{
+
+		HashMap<Integer, String> names = new HashMap<Integer, String>();
+		String name = "";
+
+		for (int i = offset; i < this.loadedFile.length && (i < tableLength + offset); i++)
+		{
+			short currentByte = (short) (this.loadedFile[i] & 0xFF);
+			// new word starts and is not the beginning of reading
+			if (currentByte == 0x00 && !name.isEmpty())
+			{
+				// offset = currentPosition - length of string (ASCII sign ==
+				// one byte)
+				names.put(i - name.length(), name);
+				name = "";
+			}
+			else if (!(currentByte == 0x00))
+			{
+				// build string
+				name = name + (char) currentByte;
+			}
+		}
+
+		return names;
+
+	}
+
+
+
+
+	/**
+	 * Prints loaded Bytes as HEX.
+	 */
+	private void printBytesAsHex()
+	{
+		String output = "";
+		if (this.loadedFile == null)
+		{
+			System.out.print("Nothing was loaded!\n");
+		}
+
+		for (int i = 0; i < this.loadedFile.length; i++)
+		{
+			if (i % 16 == 0)
+				System.out.print("\n");
+
+			output = Integer.toHexString(this.loadedFile[i] & 0xFF).toUpperCase();
+
+			if (output.length() == 1)
+				output = "0" + output;
+
+			System.out.print(output + " ");
+		}
+
+		System.out.print("\n");
+	}
+
+
+
+
+	/**
+	 * Calculates a value by shifting. Source is loadedFile. Format is little endian.
+	 * 
+	 * @param offset
+	 * @param length of bytes to read
+	 * @return -1 if error occurs
+	 */
+	private long getValue(int offset, int length)
+	{
+		if (this.loadedFile == null || this.loadedFile.length < (offset + length))
+			return -1;
+
+		long tempShift = 0;
+		long result = 0;
+		int counter = 0;
+
+		for (int i = offset; i < this.loadedFile.length && counter < length; i++)
+		{
+			if (counter != 0)
+			{
+				tempShift = this.loadedFile[i] & 0xFF;
+				result |= tempShift << (8 * counter);
+			}
+			else
+			{
+				result |= this.loadedFile[i] & 0xFF;
+			}
+			counter++;
+
+		}
+		return result;
+
+	}
+
+
+
+
+	/**
+	 * Creates an array of value. Format is little endian. Size is always 4.
+	 * 
+	 * @param value
+	 * @return byteArray in little endian format
+	 */
+	private byte[] createLittleEndianIntArrayByValue(int value)
+	{
+		byte[] temp = ByteBuffer.allocate(4).putInt(value).array();
+
+		byte[] result = new byte[4];
+
+		// to little endian
+		for (int i = 0; i < result.length; i++)
+		{
+			result[i] = temp[result.length - i - 1];
+
+		}
+
+		return result;
+	}
+
+
+
+
+	/**
+	 * Handles errors.
+	 * 
+	 * @param error
+	 */
+	private void errorHandler(ELFErrorCode error)
+	{
+		switch (error)
+		{
+		case ELF_FILE_TOO_LARGE:
+			System.out.println("ELFLoader: File is too large!\n");
+			break;
+		case ELF_PARSE_HEADER_ERROR:
+			System.out.println("ELFLoader: Can't parse header!\n");
+			break;
+		case ELF_PARSE_SECTION_HEADERS_ERROR:
+			System.out.println("ELFLoader: Can't parse section headers!\n");
+			break;
+		case ELF_PARSE_SECTION_HEADERS_NAMES_ERROR:
+			System.out.println("ELFLoader: Can't parse section header names!\n");
+			break;
+		case ELF_PARSE_SYMTABLE_ERROR:
+			System.out.println("ELFLoader: Can't parse symbol table!\n");
+			break;
+		case ELF_PARSE_SYMBTABLE_ENTRY_NAMES_ERROR:
+			System.out.println("ELFLoader: Can't parse symbol table entry names!\n");
+			break;
+		case ELF_SYMTABLE_SECTION_HEADER_NOT_FOUND_ERROR:
+			System.out.println("ELFLoader: Can't find symbol table section header!\n");
+			break;
+		case ELF_SYMTABLE_NAMES_SECTION_HEADER_NOT_FOUND_ERROR:
+			System.out.println("ELFLoader: Can't find section header of symbol table entry names!\n");
+			break;
+		default:
+			System.out.println("ELFLoader: Undefined error!\n");
+			break;
+		}
+	}
+
+
+
+
+	/**
+	 * @return the sectionDataArrays
+	 */
+	public ArrayList<byte[]> getSectionDataArrays()
+	{
+		return sectionDataArrays;
+	}
+
+
+
+
+	/**
+	 * @return the header
+	 */
+	public Header getHeader()
+	{
+		return header;
+	}
+
+
+
+
+}
---
+++ src/de/rwth/comsys/elf/SectionHeader.java
@@ -0,0 +1,333 @@
+package de.rwth.comsys.elf;
+
+/**
+ * Data structure reflects a section header of ELF. value = -1 is equivalent to null. Info:
+ * http://www.skyfree.org/linux/references/ELF_Format.pdf
+ * 
+ * @author Christian & Stephan
+ * 
+ */
+public class SectionHeader
+{
+
+	private String name;
+	private long offsetInStringTable; // name
+	private long type;
+	private long flags;
+	private long address;
+	private long sectionFileOffset;
+	private long sectionSize;
+	private long linkToAnotherSection;
+	private long info;
+	private long alignment;
+	private long fixedTableEntrySize;
+
+
+
+
+	/**
+	 * Constructor. Initializes variables with -1 (null).
+	 */
+	public SectionHeader()
+	{
+		this.setName("");
+		this.offsetInStringTable = -1;
+		this.type = -1;
+		this.flags = -1;
+		this.address = -1;
+		this.setSectionFileOffset(-1);
+		this.sectionSize = -1;
+		this.linkToAnotherSection = -1;
+		this.info = -1;
+		this.alignment = -1;
+		this.fixedTableEntrySize = -1;
+	}
+
+
+
+
+	/**
+	 * Checks if all attributes are set.
+	 * 
+	 * @return true if all attributes != -1
+	 */
+	public boolean hasNoNullAttribute()
+	{
+		if (this.offsetInStringTable == -1)
+			return false;
+		if (this.type == -1)
+			return false;
+		if (this.flags == -1)
+			return false;
+		if (this.address == -1)
+			return false;
+		if (this.getSectionFileOffset() == -1)
+			return false;
+		if (this.sectionSize == -1)
+			return false;
+		if (this.linkToAnotherSection == -1)
+			return false;
+		if (this.info == -1)
+			return false;
+		if (this.alignment == -1)
+			return false;
+		if (this.fixedTableEntrySize == -1)
+			return false;
+
+		return true;
+	}
+
+
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+
+
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+
+
+
+	/**
+	 * @return the sectionFileOffset
+	 */
+	public long getSectionFileOffset()
+	{
+		return sectionFileOffset;
+	}
+
+
+
+
+	/**
+	 * @param sectionFileOffset
+	 *            the sectionFileOffset to set
+	 */
+	public void setSectionFileOffset(long sectionFileOffset)
+	{
+		this.sectionFileOffset = sectionFileOffset;
+	}
+
+
+
+
+	/**
+	 * @return the sectionSize
+	 */
+	public long getSectionSize()
+	{
+		return sectionSize;
+	}
+
+
+
+
+	/**
+	 * @param sectionSize
+	 *            the sectionSize to set
+	 */
+	public void setSectionSize(long sectionSize)
+	{
+		this.sectionSize = sectionSize;
+	}
+
+
+
+
+	/**
+	 * @return the offsetInStringTable
+	 */
+	public long getOffsetInStringTable()
+	{
+		return offsetInStringTable;
+	}
+
+
+
+
+	/**
+	 * @param offsetInStringTable
+	 *            the offsetInStringTable to set
+	 */
+	public void setOffsetInStringTable(long offsetInStringTable)
+	{
+		this.offsetInStringTable = offsetInStringTable;
+	}
+
+
+
+
+	/**
+	 * @return the type
+	 */
+	public long getType()
+	{
+		return type;
+	}
+
+
+
+
+	/**
+	 * @param type
+	 *            the type to set
+	 */
+	public void setType(long type)
+	{
+		this.type = type;
+	}
+
+
+
+
+	/**
+	 * @return the flags
+	 */
+	public long getFlags()
+	{
+		return flags;
+	}
+
+
+
+
+	/**
+	 * @param flags
+	 *            the flags to set
+	 */
+	public void setFlags(long flags)
+	{
+		this.flags = flags;
+	}
+
+
+
+
+	/**
+	 * @return the address
+	 */
+	public long getAddress()
+	{
+		return address;
+	}
+
+
+
+
+	/**
+	 * @param address
+	 *            the address to set
+	 */
+	public void setAddress(long address)
+	{
+		this.address = address;
+	}
+
+
+
+
+	/**
+	 * @return the linkToAnotherSection
+	 */
+	public long getLinkToAnotherSection()
+	{
+		return linkToAnotherSection;
+	}
+
+
+
+
+	/**
+	 * @param linkToAnotherSection
+	 *            the linkToAnotherSection to set
+	 */
+	public void setLinkToAnotherSection(long linkToAnotherSection)
+	{
+		this.linkToAnotherSection = linkToAnotherSection;
+	}
+
+
+
+
+	/**
+	 * @return the info
+	 */
+	public long getInfo()
+	{
+		return info;
+	}
+
+
+
+
+	/**
+	 * @param info
+	 *            the info to set
+	 */
+	public void setInfo(long info)
+	{
+		this.info = info;
+	}
+
+
+
+
+	/**
+	 * @return the alignment
+	 */
+	public long getAlignment()
+	{
+		return alignment;
+	}
+
+
+
+
+	/**
+	 * @param alignment
+	 *            the alignment to set
+	 */
+	public void setAlignment(long alignment)
+	{
+		this.alignment = alignment;
+	}
+
+
+
+
+	/**
+	 * @return the fixedTableEntrySize
+	 */
+	public long getFixedTableEntrySize()
+	{
+		return fixedTableEntrySize;
+	}
+
+
+
+
+	/**
+	 * @param fixedTableEntrySize
+	 *            the fixedTableEntrySize to set
+	 */
+	public void setFixedTableEntrySize(long fixedTableEntrySize)
+	{
+		this.fixedTableEntrySize = fixedTableEntrySize;
+	}
+
+}
