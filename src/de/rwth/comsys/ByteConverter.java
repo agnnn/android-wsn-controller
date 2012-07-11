611c1d802f7a7d0e3723a48e2d5e1272bee32318
mailtotheis <mailtotheis@gmail.com>
2012-07-11 19:37:16 +0000

    elf to ihex

--- src/de/rwth/comsys/ByteConverter.java
+++ src/de/rwth/comsys/ByteConverter.java
@@ -4,79 +4,12 @@ import java.nio.ByteBuffer;
 import java.nio.ShortBuffer;
 import java.util.Formatter;
 
-import android.widget.TextView;
+
 
 public class ByteConverter {
-	static TextView tve;
-	static void bla(TextView tv)
-	{
-		tve = tv;
-	}
 	
-	static short[] getByteStreamFromSigned(ByteBuffer data)
-	{
-		/*
-		 * print the received data stream for debug purpose
-		 */
-		tve.append("\ndbg output\n");
-		//ShortBuffer sb = data.asShortBuffer();
-		for(int i=0;i<data.capacity();i++)
-		{
-			tve.append(data.get(i)+",");
-			
-		}
-		short[] resultArr = new short[data.capacity()];
-		short nextByte = 0;
-		if(data.capacity() > 0)
-			nextByte = (short)data.get(0);
-		for (int i=1;i<data.capacity();i++) {
-			short previousByte = data.get(i);
-			//System.out.println("previousByte: "+String.valueOf(previousByte));
-			//System.out.println("nextByte: "+String.valueOf(nextByte));
-			//tve.append("firstByte: "+previousByte+"\n");
-			//tve.append("nextByte: "+nextByte+"\n");
-			byte sign = (byte) (nextByte & 1);
-			short result = 0;
-			if(sign == 1)
-			{
-				//System.out.println("invert!");
-				result = getOneComplement((byte)previousByte);
-			}
-			else
-			{
-				//System.out.println("no invert!");
-				result = (short)(previousByte);
-			}
-			
-			//System.out.println("result: "+String.valueOf(result));
-			resultArr[i-1] = result;
-			previousByte = (byte)(nextByte >> 1);
-		}
-		
-		return resultArr;
-	}
 	
-	static ByteBuffer getByteBufferFromShort(short[] data)
-	{
-		ByteBuffer buf = ByteBuffer.allocate(data.length);
-		for (short dataElem : data) {
-			//byte highByte = (byte)(dataElem << 8);
-			byte lowByte = (byte)dataElem;
-			buf.put(lowByte);
-			//buf.put(highByte);
-		}
-		return buf;
-	}
 	
-	static short getOneComplement(byte byteVal)
-	{
-		return (short)(((~byteVal) + 1) & 0xFF);
-	}
-	
-	static short getOneComplement(short byteVal)
-	{
-		return (short)(((~byteVal) + 1) & 0xFFFF);
-	}
 	
 	
 	/**
@@ -84,13 +17,30 @@ public class ByteConverter {
 	 * @param data
 	 * @return
 	 */
-	static byte[] convertShortArrayToByteArray(short[] data)
+	public static byte[] convertShortArrayToByteArray(short[] data)
 	{
 		byte[] result = new byte[data.length];	
 		
 		for(int i = 0; i < data.length; i++)
 		{
 			result[i]= (byte) (data[i] & 0xFF);
+		}
+		
+		return result;
+	}	
+	
+	/**
+	 * Converts an array of bytes to an array of short.
+	 * @param data
+	 * @return
+	 */
+	public static short[] convertByteArrayToShortArray(byte[] data)
+	{
+		short[] result = new short[data.length];	
+		
+		for(int i = 0; i < data.length; i++)
+		{
+			result[i] = (short) (data[i] & 0xFF);
 		}
 		
 		return result;
