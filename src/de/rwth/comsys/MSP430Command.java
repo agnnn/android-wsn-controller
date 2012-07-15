456cc0f38aa6ba0feac6bdfd71237d2c4d17bc65
mailtotheis <mailtotheis@gmail.com>
2012-07-15 12:24:07 +0000

    kleine Änderungen

--- src/de/rwth/comsys/MSP430Command.java
+++ src/de/rwth/comsys/MSP430Command.java
@@ -2,12 +2,14 @@ package de.rwth.comsys;
 
 import java.util.ArrayList;
 
-import de.rwth.comsys.Enums.FTDI232BM_Matching_MSP430_Baudrates;
-import de.rwth.comsys.Enums.MSP430Variant;
-import de.rwth.comsys.Enums.MSP430_Commands;
+import de.rwth.comsys.enums.FTDI232BM_Matching_MSP430_Baudrates;
+import de.rwth.comsys.enums.MSP430Variant;
+import de.rwth.comsys.enums.MSP430_Commands;
+import de.rwth.comsys.ihex.Record;
 
-public class MSP430Command {
-	
+public class MSP430Command
+{
+
 	private MSP430_Commands command;
 	private byte[] data;
 	private int startAddress;
@@ -15,8 +17,13 @@ public class MSP430Command {
 	private short length;
 	private FTDI232BM_Matching_MSP430_Baudrates baudrate;
 	private MSP430Variant variant;
-	
-	public MSP430Command(MSP430_Commands command, byte[] data, int startAddress, ArrayList<Record> records, short length, FTDI232BM_Matching_MSP430_Baudrates baudrate, MSP430Variant variant ) {
+
+
+
+
+	public MSP430Command(MSP430_Commands command, byte[] data, int startAddress, ArrayList<Record> records,
+			short length, FTDI232BM_Matching_MSP430_Baudrates baudrate, MSP430Variant variant)
+	{
 		this.command = command;
 		this.data = data;
 		this.setStartAddress(startAddress);
@@ -25,135 +32,211 @@ public class MSP430Command {
 		this.baudrate = baudrate;
 		this.variant = variant;
 	}
-	
-	public MSP430Command(MSP430_Commands command) {
+
+
+
+
+	public MSP430Command(MSP430_Commands command)
+	{
 		this(command, null, 0, null, (short) 0, null, null);
 	}
-	
-	public MSP430Command(MSP430_Commands command, byte[] data) {
+
+
+
+
+	public MSP430Command(MSP430_Commands command, byte[] data)
+	{
 		this(command, data, 0, null, (short) 0, null, null);
 	}
-	
-	public MSP430Command(MSP430_Commands command, int startAddress) {
+
+
+
+
+	public MSP430Command(MSP430_Commands command, int startAddress)
+	{
 		this(command, null, startAddress, null, (short) 0, null, null);
 	}
-	
-	public MSP430Command(MSP430_Commands command, ArrayList<Record> records) {
+
+
+
+
+	public MSP430Command(MSP430_Commands command, ArrayList<Record> records)
+	{
 		this(command, null, 0, records, (short) 0, null, null);
 	}
-	
-	public MSP430Command(MSP430_Commands command, int startAddress, short length) {
+
+
+
+
+	public MSP430Command(MSP430_Commands command, int startAddress, short length)
+	{
 		this(command, null, startAddress, null, length, null, null);
 	}
-	
-	public MSP430Command(MSP430_Commands command, FTDI232BM_Matching_MSP430_Baudrates baudrate, MSP430Variant variant) {
+
+
+
+
+	public MSP430Command(MSP430_Commands command, FTDI232BM_Matching_MSP430_Baudrates baudrate, MSP430Variant variant)
+	{
 		this(command, null, 0, null, (short) 0, baudrate, variant);
 	}
 
-	
-	
+
+
+
 	/**
 	 * @return the command
 	 */
-	public MSP430_Commands getCommand() {
+	public MSP430_Commands getCommand()
+	{
 		return command;
 	}
-	
+
+
+
+
 	/**
 	 * @param command the command to set
 	 */
-	public void setCommand(MSP430_Commands command) {
+	public void setCommand(MSP430_Commands command)
+	{
 		this.command = command;
 	}
-	
+
+
+
+
 	/**
 	 * @return the data
 	 */
-	public byte[] getData() {
+	public byte[] getData()
+	{
 		return data;
 	}
-	
+
+
+
+
 	/**
 	 * @param data the data to set
 	 */
-	public void setData(byte[] data) {
+	public void setData(byte[] data)
+	{
 		this.data = data;
 	}
+
+
+
+
 	/**
 	 * @return the startAddress
 	 */
-	
 
 	/**
 	 * @return the records
 	 */
-	public ArrayList<Record> getRecords() {
+	public ArrayList<Record> getRecords()
+	{
 		return records;
 	}
+
+
+
 
 	/**
 	 * @param records the records to set
 	 */
-	public void setRecords(ArrayList<Record> records) {
+	public void setRecords(ArrayList<Record> records)
+	{
 		this.records = records;
 	}
+
+
+
 
 	/**
 	 * @return the startAddress
 	 */
-	public int getStartAddress() {
+	public int getStartAddress()
+	{
 		return startAddress;
 	}
+
+
+
 
 	/**
 	 * @param startAddress the startAddress to set
 	 */
-	public void setStartAddress(int startAddress) {
+	public void setStartAddress(int startAddress)
+	{
 		this.startAddress = startAddress;
 	}
+
+
+
 
 	/**
 	 * @return the length
 	 */
-	public short getLength() {
+	public short getLength()
+	{
 		return length;
 	}
+
+
+
 
 	/**
 	 * @param length the length to set
 	 */
-	public void setLength(short length) {
+	public void setLength(short length)
+	{
 		this.length = length;
 	}
+
+
+
 
 	/**
 	 * @return the baudrate
 	 */
-	public FTDI232BM_Matching_MSP430_Baudrates getBaudrate() {
+	public FTDI232BM_Matching_MSP430_Baudrates getBaudrate()
+	{
 		return baudrate;
 	}
+
+
+
 
 	/**
 	 * @param baudrate the baudrate to set
 	 */
-	public void setBaudrate(FTDI232BM_Matching_MSP430_Baudrates baudrate) {
+	public void setBaudrate(FTDI232BM_Matching_MSP430_Baudrates baudrate)
+	{
 		this.baudrate = baudrate;
 	}
+
+
+
 
 	/**
 	 * @return the variant
 	 */
-	public MSP430Variant getVariant() {
+	public MSP430Variant getVariant()
+	{
 		return variant;
 	}
+
+
+
 
 	/**
 	 * @param variant the variant to set
 	 */
-	public void setVariant(MSP430Variant variant) {
+	public void setVariant(MSP430Variant variant)
+	{
 		this.variant = variant;
 	}
-	
-	
 
 }
--- src/de/rwth/comsys/SocketService.java
+++ src/de/rwth/comsys/SocketService.java
@@ -6,49 +6,57 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import de.rwth.comsys.helpers.IOHandler;
+
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.widget.Toast;
 
-public class SocketService extends Service {
+public class SocketService extends Service
+{
 	private ServerSocket inOutSocket;
 	private Socket mySock;
 	private static FTDI_Interface ftdiInterface;
 	private OutputStream outStream;
 	private InputStream inStream;
 	private static AndroidWSNControllerActivity context;
-	
+
 	final int READ_CYCLE_TIMEOUT = 1000; // time between 2 read cycles
 	final int WRITE_TIMEOUT = 100; // timeout for a write operation
 	final int READ_TIMEOUT = 100;
 	final int MAX_BUFFER_SIZE = 255;
 	private volatile boolean stopped;
-	
+
+
+
+
 	@Override
-	public void onStart(Intent intent, int startid) {
+	public void onStart(Intent intent, int startid)
+	{
 		Toast.makeText(this, "My Service Started", Toast.LENGTH_LONG).show();
 		IOHandler.doOutput("service started!");
-		try {
+		try
+		{
 			mySock = inOutSocket.accept();
 			outStream = mySock.getOutputStream();
 			outStream.flush();
 			inStream = mySock.getInputStream();
 			byte[] buffer = new byte[MAX_BUFFER_SIZE];
 			int readBytes = 0;
-			while(!mySock.isClosed() && !stopped) // as long as there is a listener
+			while (!mySock.isClosed() && !stopped) // as long as there is a listener
 			{
 				IOHandler.doOutput("start forwarding");
 				// first perform a write operation from the user to the mote
 				readBytes = inStream.read(buffer);
-				if(readBytes != -1)
+				if (readBytes != -1)
 				{
 					ftdiInterface.write(buffer, WRITE_TIMEOUT);
 				}
 				// now send the data from the mote to the user
 				byte[] readMoteData = ftdiInterface.read(100);
 				// if some data is received, forward it to the user socket
-				if(readMoteData != null)
+				if (readMoteData != null)
 				{
 					IOHandler.doOutput("there is data to forward:");
 					outStream.write(readMoteData);
@@ -58,48 +66,69 @@ public class SocketService extends Service {
 				{
 					IOHandler.doOutput("no data from mote");
 				}
-				
+
 				Thread.sleep(READ_CYCLE_TIMEOUT);
 			}
-			
+
 			mySock.close();
-			
-		} catch (IOException e) {
+
+		} catch (IOException e)
+		{
 			IOHandler.doOutput(e.getMessage());
 		} // this thread waits for a connection
-		catch (InterruptedException e) {
+		catch (InterruptedException e)
+		{
 			IOHandler.doOutput(e.getMessage());
 			e.printStackTrace();
 		}
-		
-		// if everything went well there is a need to open the socket again?! 
+
+		// if everything went well there is a need to open the socket again?!
 	}
 
+
+
+
 	@Override
-	public IBinder onBind(Intent arg0) {
+	public IBinder onBind(Intent arg0)
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+
+
+
 	@Override
-	public void onDestroy() {
+	public void onDestroy()
+	{
 		Toast.makeText(context, "My Service Stopped", Toast.LENGTH_LONG).show();
 		stopped = true;
 	}
 
-	public static void setInterface(FTDI_Interface ftdiInterface2) {
+
+
+
+	public static void setInterface(FTDI_Interface ftdiInterface2)
+	{
 		ftdiInterface = ftdiInterface2;
 		Toast.makeText(context, "added interface", Toast.LENGTH_LONG).show();
 	}
 
+
+
+
 	@Override
-	public void onCreate() {
+	public void onCreate()
+	{
 		Toast.makeText(context, "Created", Toast.LENGTH_LONG).show();
 	}
 
-	public static void setContext(AndroidWSNControllerActivity context2) {
+
+
+
+	public static void setContext(AndroidWSNControllerActivity context2)
+	{
 		context = context2;
 	}
-	
-	
+
 }
