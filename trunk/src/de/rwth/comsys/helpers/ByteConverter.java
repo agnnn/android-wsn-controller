2e2fe0eaaa7e7985bce63e2f9fa8f2b8242956ef
mailtotheis@gmail.com <mailtotheis@gmail.com@842361ae-13d9-ffc3-ab2c-1d8eb7f94f8e>
2012-07-15 12:23:14 +0000

    kleine Änderungen

---
+++ trunk/src/de/rwth/comsys/helpers/ByteConverter.java
@@ -0,0 +1,45 @@
+package de.rwth.comsys.helpers;
+
+public class ByteConverter
+{
+
+	/**
+	 * Converts an array of short to an array of bytes.
+	 * 
+	 * @param data
+	 * @return
+	 */
+	public static byte[] convertShortArrayToByteArray(short[] data)
+	{
+		byte[] result = new byte[data.length];
+
+		for (int i = 0; i < data.length; i++)
+		{
+			result[i] = (byte) (data[i] & 0xFF);
+		}
+
+		return result;
+	}
+
+
+
+
+	/**
+	 * Converts an array of bytes to an array of short.
+	 * 
+	 * @param data
+	 * @return
+	 */
+	public static short[] convertByteArrayToShortArray(byte[] data)
+	{
+		short[] result = new short[data.length];
+
+		for (int i = 0; i < data.length; i++)
+		{
+			result[i] = (short) (data[i] & 0xFF);
+		}
+
+		return result;
+	}
+
+}
